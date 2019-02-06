a6c4557779cd77669f4cefa0a4618070298db14d
thierry <thierry_tct@yahoo.com>
2019-02-06 17:48:23 +0100

    part controller checkpt

--- muteria/controller/checkpoint_tasks.py
+++ muteria/controller/checkpoint_tasks.py
@@ -34,6 +34,13 @@ class Tasks(enum.Enum):
     FINISHING = "FINISHING"
 #~ class Tasks
 
+@enum.unique
+class Status(enum.Enum):
+    UNTOUCHED = -1
+    EXECUTING = 0
+    DONE = 1
+#~ class Status
+
 class TaskOrderingDependency(object): 
     '''
     The task dependency structure is following(Teh structure neve change):
@@ -106,7 +113,7 @@ class TaskOrderingDependency(object):
                         done_finishing=False):
         # set the root cell (The root has no uses)
         finishing = Cell(Task.FINISHING)
-        finishing.set_is_done(done_finishing)
+        finishing.set_status(done_finishing)
 
         self.root = finishing
 
@@ -114,52 +121,52 @@ class TaskOrderingDependency(object):
         ## Stats Layer
         ### Aggregate stats
         agg_stats =  Cell(Task.AGGREGATED_STATS)
-        agg_stats.set_is_done(done_aggregated_stats)        
+        agg_stats.set_status(done_aggregated_stats)        
         finishing.add_dependency(agg_stats)
 
         ### Other stats
         passfail_stats = Cell(Task.PASS_FAIL_STATS)
-        passfail_stats.set_is_done(done_passfail_stats)
+        passfail_stats.set_status(done_passfail_stats)
         agg_stats.add_dependency(passfail_stats)
 
         cc_stats = Cell(Task.CODE_COVERAGE_STATS)
-        cc_stats.set_is_done(done_code_coverage_stats)
+        cc_stats.set_status(done_code_coverage_stats)
         agg_stats.add_dependency(cc_stats)
 
         mc_stats = Cell(Task.MUTANT_COVERAGE_STATS)
-        mc_stats.set_is_done(done_mutant_coverage_stats)
+        mc_stats.set_status(done_mutant_coverage_stats)
         agg_stats.add_dependency(mc_stats)
 
         wm_stats = cell(Task.WEAK_MUTATION_STATS)
-        wm_stats.set_is_done(done_weak_mutation_stats)
+        wm_stats.set_status(done_weak_mutation_stats)
         agg_stats.add_dependency(wm_stats)
 
         sm_stats = Cell(Task.STRONG_MUTATION_STATS)
-        sm_stats.set_is_done(done_strong_mutation_stats)
+        sm_stats.set_status(done_strong_mutation_stats)
         agg_stats.add_dependency(sm_stats)
 
         ## Matrices Layer
         sm = Cell(Task.STRONG_MUTATION_TESTS_EXECUTION)
-        sm.set_is_done(done_strong_mutant_execution)
+        sm.set_status(done_strong_mutant_execution)
         sm_stats.add_dependency(sm)
 
         cc = Cell(Task.CODE_COVERAGE_TESTS_EXECUTION)
-        cc.set_is_done(done_code_coverage_execution)
+        cc.set_status(done_code_coverage_execution)
         cc_stats.add_dependency(cc)
         sm.add_dependency(cc)
 
         mc = Cell(Task.MUTANT_COVERAGE_TESTS_EXECUTION)
-        mc.set_is_done(done_mutant_coverage_execution)
+        mc.set_status(done_mutant_coverage_execution)
         mc_stats.add_dependency(mc)
         sm.add_dependency(mc)
 
         wm = Cell(Task.WEAK_MUTATION_TESTS_EXECUTION)
-        wm.set_is_done(done_weak_mutant_execution)
+        wm.set_status(done_weak_mutant_execution)
         wm_stats.add_dependency(wm)
         sm.add_dependency(wm)
 
         passfail = Cell(Task.PASS_FAIL_TESTS_EXECUTION)
-        passfail.set_is_done(done_test_passfail_execution)
+        passfail.set_status(done_test_passfail_execution)
         passfail_stats.add_dependency(passfail)
         cc.add_dependency(passfail)
         mc.add_dependency(passfail)
@@ -167,34 +174,34 @@ class TaskOrderingDependency(object):
 
         # Artifact Selection and Prioritization
         cs_sp = Cell(Task.CODE_SCOPE_SELECTION_PRIORITIZATION)
-        cs_sp.set_is_done(done_code_scope_selection_prioritization)
+        cs_sp.set_status(done_code_scope_selection_prioritization)
         cc.add_dependency(cs_sp)
 
         t_sp = Cell(Task.TESTS_SELECTION_PRIORITIZATION)
-        t_sp.set_is_done(done_tests_selection_prioritization)
+        t_sp.set_status(done_tests_selection_prioritization)
         passfail.add_dependency(t_sp)
 
         m_sp = Cell(Task.MUTANTS_SELECTION_PRIORITIZATION)
-        m_sp.set_is_done(done_mutant_selection_prioritization)
+        m_sp.set_status(done_mutant_selection_prioritization)
         wm.add_dependency(m_sp)
         mc.add_dependency(m_sp)
 
         # Artifact Generation
         cs_gen = Cell(Task.CODE_SCOPE_GENERATION)
-        cs_gen.set_is_done(done_code_scope_generation)
+        cs_gen.set_status(done_code_scope_generation)
         cs_sp.add_dependency(cs_gen)
 
         t_gen = Cell(Task.TESTS_GENERATION)
-        t_gen.set_is_done(done_test_generation)
+        t_gen.set_status(done_test_generation)
         t_sp.add_dependency(t_gen)
 
         m_gen = Cell(Task.MUTANTS_GENERATION)
-        m_gen.set_is_done(done_mutant_generation)
+        m_gen.set_status(done_mutant_generation)
         m_sp.add_dependency(m_gen)
 
         # Starting
         starting = Cell(STARTING)
-        starting.set_is_done(done_starting)
+        starting.set_status(done_starting)
         cs_gen.add_dependency(starting)
         t_gen.add_dependency(starting)
         m_gen.add_dependency(starting)
@@ -211,19 +218,24 @@ class TaskOrderingDependency(object):
             error_handler.error_exit()
     #~ def __init__()
 
-    def completed_task (self, task_name):
+    def complete_task (self, task_name):
         # look the task up from the root
         t = self._lookup_task_cell(task_name)
         # Check that all deps are done
         self._recursive_check_deps_are_done(t)
         # set it to complete
-        t.set_is_done(True)
-    #~ def completed_task()
+        t.set_status(True)
+    #~ def complete_task()
 
     def task_is_complete(self, task_name):
         t = self._lookup_task_cell(task_name)
         return t.is_done()
     #~ def task_is_complete()
+
+    def task_is_executing(self, task_name):
+        t = self._lookup_task_cell(task_name)
+        return t.is_executing()
+    #~ def task_is_executing()
 
     def get_next_todo_tasks(self):
         task_node_set = set()
@@ -235,18 +247,18 @@ class TaskOrderingDependency(object):
         return task_set
     #~ def get_next_todo_tasks()
 
-    def set_task_back_as_todo(self, task_name):
+    def set_task_back_as_todo(self, task_name, set_untouch=True):
         t = self._lookup_task_cell(task_name)
-        self._recursive_set_task_back_as_todo(t)
+        self._recursive_set_task_back_as_todo(t, set_untouch=set_untouched)  # TODO TODO: Also chanck the use of all functions in Cell
     #~ def set_task_back_as_todo()
 
     # ==== PRIVATE ====
 
     class Cell():
-        def __init__(self, task_name, is_done=False):
+        def __init__(self, task_name, status=UNTOUCH):
             self.dependencies = set()
             self.uses = set()
-            self.is_done = is_done
+            self.status = status
             self.task_name = task_name
 
         # setters
@@ -254,17 +266,30 @@ class TaskOrderingDependency(object):
             self.dependencies.add(cell)
         def add_use(self, cell):
             self.uses.add(cell)
-        def set_is_done(self, is_done):
-            self.is_done = is_done
+        def set_status(self, status):
+            self.status = status
+        def set_done(self):
+            self.status = Status.DONE
+        def set_executing(self):
+            self.status = Status.EXECUTING
+        def set_untouched(self):
+            self.status = Status.UNTOUCHED
         # getters
         def get_dependencies(self):
             return self.dependencies
         def get_uses(self):
             return self.uses
-        def get_is_done(self):
-            return self.is_done
+        def get_status(self):
+            return self.status
         def get_task_name(self):
             return self.task_name
+        # extra getters
+        def is_done(self):
+            return self.status == Status.DONE
+        def is_executing(self):
+            return self.status == Status.EXECUTING
+        def is_untouched(self):
+            return self.status == Status.UNTOUCHED
     #~ class Cell
 
     def _compute_uses_recursive(self, start_node, visited):
@@ -304,7 +329,7 @@ class TaskOrderingDependency(object):
     def _recursive_check_deps_are_done(self, start_node):
         problem = None
         for d in start_node.get_dependencies():
-            if not d.get_is_done():
+            if not d.is_done():
                 problem = d.get_task_name
                 break
             self._recursive_check_deps_are_done(d)
@@ -315,18 +340,18 @@ class TaskOrderingDependency(object):
     #~ def _recursive_check_deps_are_done()
 
     def _recursive_set_task_back_as_todo(self, start_node):
-        if start_node.is_done():
-            start_node.set_is_done(False)
+        if not start_node.is_untouched():
+            start_node.set_status(False)
             for u in start_node.get_uses():
                 self._recursive_set_task_back_as_todo(u)
     #~ def _recursive_set_task_back_as_todo
 
     def _recursive_get_next_todo_tasks(task_set, start_node):
-        if start_node.get_is_done():
+        if start_node.is_done():
             return
         is_leaf = True
         for d in start_node.get_dependencies():
-            if not d.get_is_done():
+            if not d.is_done():
                 is_leaf = False
                 self._recursive_get_next_todo_tasks(task_set, d)
         if is_leaf:
