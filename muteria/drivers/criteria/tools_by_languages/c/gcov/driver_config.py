364852f97f3a26ef7ce818eb0c9093c46546f032
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-06-04 14:19:11 +0200

    Update driver_config.py

--- muteria/drivers/criteria/tools_by_languages/c/gcov/driver_config.py
+++ muteria/drivers/criteria/tools_by_languages/c/gcov/driver_config.py
@@ -2,13 +2,19 @@ import muteria.common.mix as common_mix
 ERROR_HANDLER = common_mix.ErrorHandler
 
 class DriverConfigGCov:
-    def __init__(self, allow_missing_coverage=False, **kwargs):
+    def __init__(self, allow_missing_coverage=False, use_gdb_wrapper=True, \
+                                                                    **kwargs):
         ERROR_HANDLER.assert_true(type(allow_missing_coverage) == bool, \
                 "invalid allow_missing_coverage type. Must be bool", __file__)
         self.allow_missing_coverage = allow_missing_coverage
+        self.use_gdb_wrapper = use_gdb_wrapper
     #~ def __init__()
 
     def get_allow_missing_coverage(self):
         return self.allow_missing_coverage
     #~ def get_allow_missing_coverage()
+    
+    def get_use_gdb_wrapper(self):
+        return self.use_gdb_wrapper
+    #~ def get_use_gdb_wrapper()
 #~ class DriverConfigGCov
