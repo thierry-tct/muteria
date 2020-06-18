45dcd17ac27a70664277160ef6f2acc339e9c953
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-06-18 15:27:26 +0200

    Update shadow_se.py

--- muteria/drivers/testgeneration/tools_by_languages/c/shadow_se/shadow_se.py
+++ muteria/drivers/testgeneration/tools_by_languages/c/shadow_se/shadow_se.py
@@ -249,7 +249,7 @@ class TestcasesToolShadowSE(TestcasesToolKlee):
             per_test_hard_timeout = per_test_timeout + \
                                 self.config.TEST_GEN_TIMEOUT_FRAMEWORK_GRACE
 
-        #per_test_hard_timeout = 300 #DBG
+        per_test_hard_timeout = 300 #DBG
         # Set the wrapper
         with open(call_shadow_wrapper_file, 'w') as wf:
             wf.write('#! /bin/bash\n\n')
@@ -260,7 +260,7 @@ class TestcasesToolShadowSE(TestcasesToolKlee):
             # timeout the shadow execution (some test create daemon which)
             # are not killed by test timeout. ALSO MAKE SURE TO DESACTIVATE 
             # IN TEST SCRIPT TIMEOUT
-            kill_after = 30
+            kill_after = 3600
             wf.write('time_out_cmd="/usr/bin/timeout --kill-after={}s {}"\n'.\
                                      format(kill_after, per_test_hard_timeout))
             # kill after and time for timeout to act
@@ -303,8 +303,8 @@ class TestcasesToolShadowSE(TestcasesToolKlee):
                                     env_vars, timeout=per_test_hard_timeout,\
                                                     with_output_summary=False)
             
-            #logging.debug("DBG: Just executed test '{}'".format(meta_test))
-            #input(">>>> ") #DBG
+            logging.debug("DBG: Just executed test '{}'".format(meta_test))
+            input(">>>> ") #DBG
             
             # copy the klee out
             test_out = os.path.join(self.tests_storage_dir, \
