714c6fa1461a46bf1ea447adf0f02fdaa99f2fc0
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2021-11-26 19:46:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #
 #> python3 -m pip install --user --upgrade setuptools wheel
+#> python3 -m pip install --user --upgrade twine
 #
 #> python3 setup.py sdist bdist_wheel
-#> python3 -m pip install --user --upgrade twine
 #> python3 -m twine upload dist/muteria-<version>.tar.gz
 #> rm -rf dist build muteria.egg-info __pycache__/
 #
